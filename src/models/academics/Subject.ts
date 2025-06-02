31c934e111af8ef12269afab2fa7788e645b7c49
SOPHY-168 <emsophy80@gmail.com>
2025-06-02 09:38:20 +0700

    Create Teacher

---
+++ src/models/academics/Subject.ts
@@ -0,0 +1,21 @@
+export class Subject {
+  private subject_id: string
+  private subject_name: string
+  private subject_code: string
+  private credits: number
+  private description: string
+
+  constructor(
+    subject_id: string,
+    subject_name: string,
+    subject_code: string,
+    credits: number,
+    description: string,
+  ) {
+    this.subject_id = subject_id
+    this.subject_name = subject_name
+    this.subject_code = subject_code
+    this.credits = credits
+    this.description = description
+  }
+}
--- src/models/users/Teacher.ts
+++ src/models/users/Teacher.ts
@@ -1,13 +1,58 @@
-class teacher {
-  constructor(
-    public id: string,
-    public name: string,
-    public email: string,
-    public subject: string,
-    public yearsOfExperience: number
-  ) {}
+import { User } from "./User"
+import type { Subject } from "../academics/Subject"
+import type { Assignment } from "../academics/Assignment"
+export class Teacher extends User {
+  private teacherId: number
+  private specialization: string
+  private subjects: Subject[] = []
+  private assignments: Assignment[] = []
 
-  getDetails(): string {
-    return `${this.name} teaches ${this.subject} and has ${this.yearsOfExperience} years of experience.`;
+  constructor(
+    id: string,
+    name: string,
+    email: string,
+    password: string,
+    phone: string,
+    address: string,
+    teacherId: number,
+    specialization: string,
+  ) {
+    super(id, name, email, password, phone, address)
+    this.teacherId = teacherId
+    this.specialization = specialization
+  }
+
+  public getRole(): string {
+    return "Teacher"
+  }
+
+  public getTeacherId(): number {
+    return this.teacherId
+  }
+
+  public getSpecialization(): string {
+    return this.specialization
+  }
+
+  public assignSubject(subject: Subject): void {
+      this.subjects.push(subject)
+    
+  }
+
+  public getSubjects(): Subject[] {
+    return this.subjects
+  }
+
+  public getAssignments(): Assignment[] {
+    return this.assignments
+  }
+
+  public removeSubject(subject: Subject): boolean {
+    const index = this.subjects.indexOf(subject)
+    if (index > -1) {
+      this.subjects.splice(index, 1)
+      return true
+    }
+    return false
   }
 }
