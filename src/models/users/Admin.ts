c86c0d9213cd754fe6494c637d89fdff439f8d82
unknown <rachana.chhum@student.passerellesnumeriques.org>
2025-06-02 09:45:21 +0700

    fix

--- src/models/users/Admin.ts
+++ src/models/users/Admin.ts
@@ -13,7 +13,7 @@ export class Admin extends User {
     address: string,
     adminLevel: string,
   ) {
-    super(id, name, email, password, phone, address)
+    super(id,name,email,password,phone,address)
     this.adminLevel = adminLevel
   }
 
