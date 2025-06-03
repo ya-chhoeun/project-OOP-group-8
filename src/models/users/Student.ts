5746eb276e5205aab6ce276292c86b656826f507
ya-chhoeun <ya.chhoeun@student.passerellesnumeriques.org>
2025-06-03 12:30:09 +0700

    fix class user

--- src/models/users/Student.ts
+++ src/models/users/Student.ts
@@ -12,25 +12,5 @@ class Student extends User {
     this.studentId = id;
   }
 
-    // public viewTimetable(day?: string): Timetable[] {
-    //   const timetableEntry = day ? `Timetable for ${day}` : "Default Timetable";
-    //   return [new Timetable([timetableEntry])];
-    // }
-
-    // public submitAssignment(assignment: Assignment): Assignment {
-    //   console.log(`Assignment submitted by ${this.name}`);
-    //   return assignment;
-    // }
-
-    // public viewGrade(): void {
-    //   console.log(`${this.name}'s grades displayed`);
-    // }
-
-    // public viewExamSchedule(): void {
-    //   console.log(`${this.name}'s exam schedule displayed`);
-    // }
-
-    // public giveFeedback(subject: Subject, teacher: Teacher): void {
-    //   console.log(`Feedback given by ${this.name} for ${subject.getName()} taught by ${teacher.getName()}`);
-    // }
+  
 }
