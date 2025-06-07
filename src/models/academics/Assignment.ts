9db650fb015dc389b7fbde99528f83a93444efb2
ya-chhoeun <ya.chhoeun@student.passerellesnumeriques.org>
2025-06-07 13:02:29 +0700

    update code

--- src/models/academics/Assignment.ts
+++ src/models/academics/Assignment.ts
@@ -135,33 +135,6 @@ export class Assignment {
     return this.dueDate
   }
 
-//   public getMaxMarks(): number {
-//     return this.max_marks
-//   }
 
-//   public getSubject(): Subject {
-//     return this.subject
-//   }
-  
-// }
-//   public getTeacher(): Teacher {
-//     return this.teacher
-//   }
-
-//   public getCreatedDate(): Date {
-//     return this.created_date
-//   }
-
-//   public isPublished(): boolean {
-//     return this.is_published
-//   }
-
-//   public publish(): void {
-//     this.is_published = true
-//   }
-
-//   public unpublish(): void {
-//     this.is_published = false
-//   }
 }
 
